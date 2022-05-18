fc222f44c87a15be1a0461466f5cb6e11f5349a5
rafael-goldberg <rafael_goldberg@hotmail.com>
2022-05-18 09:41:34 -0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+1. Setting up the serverless framework with aws - Configure AWS Credentials
+2. Creating a serverless project and deploy lambda
+3. Deploy a s3 bucket and update data
+4. Creating an api with serverless - API Gateway and AWS Lambda
+5. Adding serverless webpack - fix lambda upload limits
+6. Create a serverless database dynamodb
+7. Create an api to get data from dynamodb
+8. Adding new data to dynamodb
+9. Adding and getting files from s3 with serverless
+10. Securing serverless api with api keys
+11. Proxy api requests to legacy endpoints
+12. Send email with SES
+13. Creating an IAM Assume Role - working with multiple aws accounts
+14. <s>Create a websocket API</s>
+15. Send an SMS text
+16. Analyse Text with Amazon Comprehend - Sentiment Analysis and Entity Extraction
+17. Executar lambdas automaticamente tarefas em determinados momentos do dia ou da semana
+18. Deploy React app with CloudFront
+19. Write Cleaner Lambdas with Lambda Hooks
---
+++ lambdas/common/DynamoDB.js
@@ -0,0 +1,47 @@
+const AWS = require('aws-sdk');
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+const Dynamo = {
+    async get (ID, TableName) {
+        const params = {
+            TableName,
+            Key: {
+                ID
+            }
+        };
+
+        const data = await documentClient
+            .get(params)
+            .promise()
+
+        if (!data || !data.Item) {
+            throw Error(`There was an error fetching the data for ID of ${ID} from ${TableName}`)
+        }
+        console.log('GET', data);
+
+        return data.Item;
+    },
+
+    async write (data, TableName) {
+        if (!data.ID) {
+            throw Error('no ID on the data')
+        }
+
+        const params = {
+            TableName,
+            Item: data
+        };
+
+        const res = await documentClient.put(params).promise();
+
+        if (!res) {
+            throw Error(`There was an error inserting ID of ${data.ID} in table ${TableName}`)
+        }
+        console.log('Write', data);
+
+        return data;
+    }
+}
+
+module.exports = Dynamo;
